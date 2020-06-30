5fda0e8d074f7c4409279c9efab42f4485dc1e12
Kos <locoprodukten@gmail.com>
2020-06-30 20:44:21 +0200

    Updates

---
+++ precache-manifest.fc2704f6c97ec8feec004d7bb06b6a5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42215706bbd9ad8f96c86483547699f3",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "b1cd0794549cb8d63e3c",
+    "url": "/react-tic-tac-toe/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "185550ba4c511e915492",
+    "url": "/react-tic-tac-toe/static/js/2.e977bf79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.e977bf79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1cd0794549cb8d63e3c",
+    "url": "/react-tic-tac-toe/static/js/main.07189078.chunk.js"
+  },
+  {
+    "revision": "d9255e6de55285e18dc3",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.81654cbd.js"
+  }
+]);
